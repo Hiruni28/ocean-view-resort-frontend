79bb75c7ab1bcf27e4090f9d8d927efe1f2ee272
Hiruni28 <hiruniparindhya28@gmail.com>
2026-02-20 06:56:53 +0530

    add vite configuration

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+*/
+/*import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ REQUIRED
+  ],
+});
+*/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ REQUIRED
+  ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
